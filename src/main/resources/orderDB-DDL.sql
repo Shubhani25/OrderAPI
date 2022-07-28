e4a2028914993f77102b854b50b03945aaaca332
Shubhani25 <shubhanimulay25@gmail.com>
2022-07-29 01:44:45 +0530

    updated sql script

--- src/main/resources/orderDB-DDL.sql
+++ src/main/resources/orderDB-DDL.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE IF NOT EXISTS public.customer_orders
 (
-    id integer NOT NULL ,
+    id integer NOT NULL DEFAULT nextval('customer_orders_id_seq'::regclass),
     customer_id integer NOT NULL,
     order_id integer NOT NULL,
     store_id integer NOT NULL,
@@ -27,7 +27,7 @@ ALTER TABLE IF EXISTS public.customer_orders
 
 CREATE TABLE IF NOT EXISTS public.order_items
 (
-    id integer NOT NULL ,
+    id integer NOT NULL DEFAULT nextval('order_items_id_seq'::regclass),
     order_id integer NOT NULL,
     item_id integer NOT NULL,
     item_price numeric NOT NULL,
